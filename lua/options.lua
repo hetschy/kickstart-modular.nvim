b3cba477b258e316eadd7738fab449d2c4284c72
Hetschy <hetsch@gmail.com>
2025-05-29 15:15:28 +0200

    cleanup

--- lua/options.lua
+++ lua/options.lua
@@ -5,19 +5,13 @@
 
 -- Make line numbers default
 vim.o.number = true
+
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-<<<<<<< HEAD
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = ''
-=======
--- vim.o.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
->>>>>>> upstream/master
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
